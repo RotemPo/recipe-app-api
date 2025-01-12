dd874d2cb1958f8f168b6f29c1f72d65a46ad04f
RotemPo <rotem.portugez@gmail.com>
2025-01-12 16:03:29 +0200

    recipe app

--- app/core/admin.py
+++ app/core/admin.py
@@ -4,7 +4,7 @@ Django admin customization.
 """
 
 from django.contrib import admin  # type: ignore # noqa
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin  # type: ignore
 from django.utils.translation import gettext_lazy as _  # type: ignore
 
 from core import models
@@ -47,3 +47,4 @@ class UserAdmin(BaseUserAdmin):
 
 
 admin.site.register(models.User, UserAdmin)
+admin.site.register(models.Recipe)
