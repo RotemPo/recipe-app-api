220a3e99b8aaaccceb62a9c1205ea956d264d233
RotemPo <rotem.portugez@gmail.com>
2025-01-12 14:24:09 +0200

    manage user API

--- app/user/urls.py
+++ app/user/urls.py
@@ -1,7 +1,7 @@
 """
 URL mappings for the user API.
 """
-from django.urls import path
+from django.urls import path # type: ignore
 
 from user import views
 
@@ -11,4 +11,5 @@ app_name = 'user'
 urlpatterns = [
     path('create/', views.CreateUserView.as_view(), name='create'),
     path('token/', views.CreateTokenView.as_view(), name='token'),
+    path('me/', views.ManageUserView.as_view(), name='me'),
 ]
