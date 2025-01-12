eb8b37faf0a0ca2b67e644a46385a9bf1191c3b0
RotemPo <rotem.portugez@gmail.com>
2025-01-12 15:12:17 +0200

    ...

--- app/core/admin.py
+++ app/core/admin.py
@@ -4,14 +4,13 @@ Django admin customization.
 """
 
 from django.contrib import admin  # type: ignore # noqa
-from django.contrib.auth.admin import   # type: ignore \
-UserAdmin as BaseUserAdmin  # type: ignore
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _  # type: ignore
 
 from core import models
 
 
-class UserAdmin(BaseUserAdmin):  # type: ignore
+class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users."""
     ordering = ['id']
     list_display = ['email', 'name']
