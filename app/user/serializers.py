dad43521faa14984dabb48e08162e0d5859b4b0d
RotemPo <rotem.portugez@gmail.com>
2025-01-12 14:03:52 +0200

    tests for maange user API

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -38,7 +38,7 @@ class AuthTokenSerializer(serializers.Serializer):
             password=password,
         )
         if not user:
-            msg = _('Unable to authenticate with provided credentials.')
+            msg = 'Unable to authenticate with provided credentials.'
             raise serializers.ValidationError(msg, code='authorization')
 
         attrs['user'] = user
